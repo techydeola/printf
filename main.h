9d9833be1f3733d1f6b2816d03aeb755c6cf5e4e
Khadija Danhassan <khadijaumardh@gmail.com>
2023-06-27 14:45:51 -0700

    more prototypes added

--- main.h
+++ main.h
@@ -6,5 +6,6 @@ int _putchar(char c);
 void print_char(char ch);
 void print_string(char *str);
 void handle_other(const char *format, va_list ap);
+int numbers(const char *format, ...);
 void print_int(int a);
 #endif
